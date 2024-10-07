829528b5903ad8ff90cb1b80005fe8384c00303f
utfpr <joaovicgomes.10@gmail.com>
2024-10-07 14:25:21 -0300

    Migrations adicionado

---
+++ EstoqueResidencial/EstoqueResidencial.Persistencia.EFCore/Migrations/20241007172346_SecondCore.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstoqueResidencial.Persistencia.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondCore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Localizacoes",
+                columns: table => new
+                {
+                    LocalizacaoID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeLocal = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Localizacoes", x => x.LocalizacaoID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Localizacoes");
+        }
+    }
+}
