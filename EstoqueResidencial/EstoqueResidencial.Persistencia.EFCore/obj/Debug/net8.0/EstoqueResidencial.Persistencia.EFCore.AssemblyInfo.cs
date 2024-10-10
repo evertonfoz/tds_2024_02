539fecf7d1f3bdf0ed663cb1f7937bdd7312352a
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2024-10-10 19:19:22 -0300

    Ajuste para gitignore

--- EstoqueResidencial/EstoqueResidencial.Persistencia.EFCore/obj/Debug/net8.0/EstoqueResidencial.Persistencia.EFCore.AssemblyInfo.cs
+++ EstoqueResidencial/EstoqueResidencial.Persistencia.EFCore/obj/Debug/net8.0/EstoqueResidencial.Persistencia.EFCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstoqueResidencial.Persistencia.EFCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea065953defcc8b3aacf24e69ae7ef9e7a911d8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e5a2d74b2a2ebe263a5c426ca6f5db0fbd0754")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstoqueResidencial.Persistencia.EFCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstoqueResidencial.Persistencia.EFCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
